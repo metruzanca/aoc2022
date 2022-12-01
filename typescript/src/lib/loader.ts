10de7ba37fa8254629188590e87e4a2d7829dec1
Sam Zanca <sam@zanca.dev>
2022-12-01 05:45:20 -0500

    docs

--- typescript/src/lib/loader.ts
+++ typescript/src/lib/loader.ts
@@ -1,6 +1,8 @@
 import { readFileSync } from 'fs';
 import { resolve } from 'path';
 
+
+/** If executing aoc solution in the browser, load can be replaced by `document.querySelector('pre').textContent` */
 export default function load(day: number) {
   const dayNumber = day.toString().padStart(2, '0');
   const path = resolve(process.cwd(), `../inputs/day${dayNumber}.txt`)
